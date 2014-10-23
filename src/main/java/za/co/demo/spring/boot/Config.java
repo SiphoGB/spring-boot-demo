4f9a89417df579a79acef080ac22e75fef2ca467
Naeem Ally <naeem@fingerprintsoft.co>
2014-10-23 12:47:13 +0200

    simple profiles using yml values

---
+++ src/main/java/za/co/demo/spring/boot/Config.java
@@ -0,0 +1,19 @@
+package za.co.demo.spring.boot;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix="config")
+public class Config {
+    private String version;
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+    
+}
--- src/main/java/za/co/demo/spring/boot/Example.java
+++ src/main/java/za/co/demo/spring/boot/Example.java
@@ -1,5 +1,6 @@
 package za.co.demo.spring.boot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Example {
 	
-	@RequestMapping("/")
+    @Autowired
+	private Config config;
+
+    @RequestMapping("/")
 	public String hello() {
 		return "java controller";
 	}
-	
+	   
+    @RequestMapping("/version")
+    public String version() {
+        return config.getVersion();
+    }
+    
 	@RequestMapping("/args/{val}")
 	public String helloWithArgs(@PathVariable("val") String val) {
 		return "Hello " + val;
