cecad1aca93ab1f344878d175c6faf01df632518
Naeem Ally <naeem@fingerprintsoft.co>
2014-11-18 10:45:22 +0200

    updated controller to use resource

--- src/main/java/za/co/demo/spring/boot/Example.java
+++ src/main/java/za/co/demo/spring/boot/Example.java
@@ -1,15 +1,21 @@
 package za.co.demo.spring.boot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import za.co.demo.spring.boot.resource.UserResource;
+
 @RestController
 public class Example {
 	
+    @Autowired
+    private UserResource userResource;
+    
 	@RequestMapping("/")
 	public String hello() {
-		return "java controller";
+		return "Hello " + userResource.findOne(1L).getName();
 	}
 	
 	@RequestMapping("/args/{val}")
