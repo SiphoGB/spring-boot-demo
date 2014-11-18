2f97a5552bba2ae516b2da80b0582cb74aeaacd4
Naeem Ally <naeem@fingerprintsoft.co>
2014-11-18 10:23:44 +0200

    updated sql

--- src/main/resources/data-dev.sql
+++ src/main/resources/data-dev.sql
@@ -1,7 +1,7 @@
 insert into topic(name, active) values ('Demo Preview', true);
-insert into question(topic_id, question) values (1, 'Who is one of the founding fathers of Spring');
-insert into answer(question_id, answer, display, correct) values (1, 'a', 'Bill Burke', false);
-insert into answer(question_id, answer, display, correct) values (1, 'b', 'R', true);
+insert into question(topic_id, question) values (1, 'Some Question');
+insert into answer(question_id, answer, display, correct) values (1, 'a', 'ABC', false);
+insert into answer(question_id, answer, display, correct) values (1, 'b', 'FNB', true);
 insert into question(topic_id, question) values (1, 'Question 2');
 insert into answer(question_id, answer, display, correct) values (2, '1', 'Answer 1 to Q 2', false);
 insert into answer(question_id, answer, display, correct) values (2, '2', 'Answer 2 to Q 2', true);
